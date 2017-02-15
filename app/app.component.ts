a3c32fd1ea0dccb7b159eb62bfafc63abb85f069
Rachel Lougee <rlougee@theadkgroup.com>
2017-02-15 10:48:56 -0500

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './templates/home.html',
+  styleUrls: [ "./css/styles.css" ]
+})
+
+export class AppComponent  {
+  name = 'Angular2';
+  content= 'This is content';
+ }
---
+++ app/login/login.component.ts
@@ -0,0 +1,12 @@
+//our root app component
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './templates/login/login.html',
+})
+export class LoginComponent {
+
+
+}
